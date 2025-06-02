61e443ad5e8924f8dc30fba7d8dfac4e2bdc28a5
Sourabh Jain <sourabh.jain@yahoo.co.in>
2025-06-02 16:59:13 -0700

    fix for issue reporter by a tester that app selection is changing while scrolling

--- app/src/main/java/com/avdhaan/AppAdapter.java
+++ app/src/main/java/com/avdhaan/AppAdapter.java
@@ -10,16 +10,24 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.DiffUtil;
 
 import java.util.List;
 
 public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
     private static final String TAG = "AppAdapter";
+    private static final int APP_TAG = 1;
     private List<AppInfo> appList;
 
     public AppAdapter(List<AppInfo> appList) {
         this.appList = appList;
+        setHasStableIds(true);
         Log.d(TAG, "Adapter created with " + appList.size() + " apps");
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return appList.get(position).getPackageName().hashCode();
     }
 
     @NonNull
@@ -32,10 +40,22 @@ public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull AppViewHolder holder, int position) {
+        // Clear previous state
+        holder.checkBox.setOnCheckedChangeListener(null);
+        
         AppInfo app = appList.get(position);
         holder.appName.setText(app.getAppName());
         holder.appIcon.setImageDrawable(app.getIcon());
+        holder.itemView.setTag(APP_TAG, app.getPackageName());
+        
+        // Set checkbox state after clearing listener
         holder.checkBox.setChecked(app.isSelected());
+
+        // Set new listener
+        holder.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            app.setSelected(isChecked);
+            Log.d(TAG, "App " + app.getAppName() + " selected: " + isChecked);
+        });
 
         holder.itemView.setOnClickListener(v -> {
             boolean newState = !app.isSelected();
@@ -43,16 +63,26 @@ public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
             holder.checkBox.setChecked(newState);
             Log.d(TAG, "App " + app.getAppName() + " selected: " + newState);
         });
+    }
 
-        holder.checkBox.setOnClickListener(v -> {
-            app.setSelected(holder.checkBox.isChecked());
-            Log.d(TAG, "App " + app.getAppName() + " selected: " + holder.checkBox.isChecked());
-        });
+    @Override
+    public void onViewRecycled(@NonNull AppViewHolder holder) {
+        super.onViewRecycled(holder);
+        holder.checkBox.setOnCheckedChangeListener(null);
+        holder.checkBox.setChecked(false);
+        holder.itemView.setTag(APP_TAG, null);
     }
 
     @Override
     public int getItemCount() {
         return appList.size();
+    }
+
+    public void updateApps(List<AppInfo> newApps) {
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new AppDiffCallback(appList, newApps));
+        appList.clear();
+        appList.addAll(newApps);
+        diffResult.dispatchUpdatesTo(this);
     }
 
     static class AppViewHolder extends RecyclerView.ViewHolder {
@@ -65,6 +95,40 @@ public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
             appIcon = itemView.findViewById(R.id.app_icon);
             appName = itemView.findViewById(R.id.app_name);
             checkBox = itemView.findViewById(R.id.checkbox_app);
+        }
+    }
+
+    private static class AppDiffCallback extends DiffUtil.Callback {
+        private final List<AppInfo> oldList;
+        private final List<AppInfo> newList;
+
+        AppDiffCallback(List<AppInfo> oldList, List<AppInfo> newList) {
+            this.oldList = oldList;
+            this.newList = newList;
+        }
+
+        @Override
+        public int getOldListSize() {
+            return oldList.size();
+        }
+
+        @Override
+        public int getNewListSize() {
+            return newList.size();
+        }
+
+        @Override
+        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldList.get(oldItemPosition).getPackageName()
+                    .equals(newList.get(newItemPosition).getPackageName());
+        }
+
+        @Override
+        public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+            AppInfo oldApp = oldList.get(oldItemPosition);
+            AppInfo newApp = newList.get(newItemPosition);
+            return oldApp.isSelected() == newApp.isSelected() &&
+                   oldApp.getAppName().equals(newApp.getAppName());
         }
     }
 } 
